aee85167502bd7238296bea86c1f40e71621a76b
Mac 10 <zecosc16@htlkaindorf.at>
2019-03-30 10:06:50 +0100

    main implementation of consumer class

--- src/main/java/htl/zechner/producerconsumerfiles/Consumer.java
+++ src/main/java/htl/zechner/producerconsumerfiles/Consumer.java
@@ -5,15 +5,62 @@
  */
 package htl.zechner.producerconsumerfiles;
 
+import Queue.EmptyException;
+import Queue.Queue;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author oskar
  */
 public class Consumer extends Thread{
 
+    private Queue<Book> queue;
+    private HashMap<String,Integer> map;
+
+    public Consumer(Queue<Book> queue) {
+        this.queue = queue;
+    }
+    
     @Override
     public void run() {
-        
+        while(true){
+            Book b = null;
+            
+            synchronized(queue){
+                try {
+                    b=queue.get();
+                } catch (EmptyException ex) {
+                    try {
+                        System.out.println(Thread.currentThread().getName()+" waits");
+                        queue.wait();
+                    } catch (InterruptedException ex1) {
+                        Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex1);
+                    }
+                    continue;
+                }
+            }
+            
+            try {
+                Thread.sleep(3000);
+                
+                map=b.countW();
+                int count=0;
+                for (String string : map.keySet()) {
+                    count=map.get(string);
+                    if(count>1)
+                        System.out.println(string+" "+count);
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
     }
     
 }
